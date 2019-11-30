979e073a5fc47f8210a5243593d5acb54355ac6a
Kangaechu <kangae2@gmail.com>
2019-11-30 15:44:48 +0900

    bury version

--- cmd/godcast/main.go
+++ cmd/godcast/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/kangaechu/godcast"
+	"os"
 )
 
+var version string
+var revision string
+
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Printf(os.Args[0]+": %s-%s\n", version, revision)
+		os.Exit(0)
+	}
+
 	confFile := flag.String("c", "podcast.yaml", "conf yaml file name")
 	flag.Parse()
 	godcast.Run(*confFile)
